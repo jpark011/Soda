ef0b07edf9f58598bf8eca0c630a99eaa5f6b8de
Jay Park <wngt3319@gmail.com>
2018-11-29 22:34:00 -0500

    Bootstrap Bank

---
+++ bank.cc
@@ -0,0 +1,18 @@
+#include "bank.h"
+
+using namespace std;
+
+Bank::Bank( unsigned int numStudents ) : numStudents(numStudents) {
+    for ( unsigned int i = 0; i < numStudents; i++ ) {
+        stdBalances.push_back(0);
+    } // for
+}
+
+void Bank::deposit( unsigned int id, unsigned int amount ) {
+    stdBalances[id] += amount;
+}
+
+void Bank::withdraw( unsigned int id, unsigned int amount ) {
+    while ( stdBalances[id] < amount ) _Accept( deposit );
+    stdBalances[id] -= amount;
+}
--- bank.h
+++ bank.h
@@ -1,7 +1,10 @@
 #ifndef __BANK_H__
 #define __BANK_H__
+#include <vector>
 
 _Monitor Bank {
+    unsigned int numStudents;
+    vector<unsigned int> stdBalances;
 public:
     Bank( unsigned int numStudents );
     void deposit( unsigned int id, unsigned int amount );
